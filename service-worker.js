adecf37fde171aed013d0c37c23a30ac9d8b678b
zaxadmin <voxrecrutement@gmail.com>
2026-06-01 00:25:10 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'zipngo-v1';
+const ASSETS = ['/', '/app.py', '/static/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
